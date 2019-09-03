51c0d766b316a1fdb0bd1e0d70949a622caf710c
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2019-09-03 17:57:16 +0200

    update docs and assertions

--- R/record.R
+++ R/record.R
@@ -1,42 +1,26 @@
 #' Record table properties
 #'
-#' This functions records properties of tables and stores them in a meta data
-#' object in preparation of rectangularisation.
+#' This functions records properties of tables and stores them in a schema
+#' description in preparation of reorganisation.
 #' @param input [\code{data.frame(1)}]\cr table for which to record properties.
-#' @param schema [\code{symbol(1)}]\cr the schema documentation that has been
+#' @param schema [\code{symbol(1)}]\cr the schema description that has been
 #'   saved in the global environment.
 #' @param ... [\code{various}]\cr property specific arguments; see Details.
-#' @details Property-specific arguments are:\itemize{ \item \code{algorithm}:
-#'   \itemize{\item \code{name = ...}} An algorithm is a list of table
-#'   properties. When calling it, \code{register} sets all of the properties
-#'   specified in the algorithm. \item \code{format:} \itemize{ \item \code{type
-#'   = "long" | "wide"} - if each variable is in one column, the table is long;
-#'   if some variables are spread over several columns, the table is wide.}
-#'   \item \code{clusters:} \itemize{\item \code{row} - the row number(s) of the
-#'   top-left corner of each cluster. \item \code{col} - the column number(s) of
-#'   the top-left corner of each cluster. \item \code{width} - the width of
-#'   clusters (in number of cells) can either have one value (when all clusters
-#'   have the same width) or as many values as there are clusters. \item
-#'   \code{height} - the height of clusters (in number of cells) can either have
-#'   one value (when all clusters have the same height) or as many values as
-#'   there are clusters.} This could for instance be \code{'row = c(2, 52), col
-#'   = 3, width = 4'}, if there are 2 rows of clusters that are 4 wide, starting
-#'   at cell 2,3 and 52,3 \item \code{commodities}: }
+#' @details Read the vignette for the basics of schema descriptions, a default
+#'   schema description is stored in \code{\link{meta_default}}.
 #' @importFrom rlang exprs
-#' @importFrom checkmate assertDataFrame assertCharacter assertChoice assertList
-#'   assertNames
+#' @importFrom checkmate assertDataFrame assertList assertNames
 #' @export
 
 record <- function(input, schema = NULL, ...){
 
   # check validity of arguments
   assertDataFrame(x = input)
-  # assertList(x = schema)
-
-  meta <- eval(parse(text = schema))
+  assertList(x = schema, len = 2)
+  assertNames(x = names(schema), permutation.of = c("clusters", "variables"))
 
   # assign the meta data object into the base-environment
-  assign(x = "meta_object", value = meta, envir = baseenv())
+  assign(x = "meta_object", value = schema, envir = baseenv())
 
 
   return(input)
