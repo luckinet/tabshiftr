1101696988ccdee4f17d85ad51212535eb19e954
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2020-03-10 19:15:44 +0100

    new variable

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,2 @@
-globalVariables(c("key", "values", "meta_default", "rn", "val", ".", "name"))
+globalVariables(c("key", "values", "meta_default", "rn", "val", ".", "name",
+                  "count"))
