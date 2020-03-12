58ae27b930908dd5ffea599e667272d72d8d3fdf
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2020-03-12 18:36:25 +0100

    update testfunction

--- R/expect_valid.R
+++ R/expect_valid.R
@@ -1,16 +1,43 @@
 #' Test for a valid table
 #' @param x a table to test.
+#' @param units the number of units in the output table (from 1 to 3)
 #' @importFrom testthat expect_identical
-#' @importFrom checkmate expect_names expect_tibble expect_list
+#' @importFrom checkmate expect_names expect_tibble expect_list assertChoice
 #' @export
 
-expect_valid_table <- function(x = NULL){
+expect_valid_table <- function(x = NULL, units = 1){
 
-  expect_tibble(x = x, any.missing = FALSE, nrows = 8, ncols = 5)
-  expect_names(x = colnames(x), permutation.of =c("territories", "year", "commodities", "harvested", "production") )
-  expect_identical(object = x$territories, expected = c("unit 1", "unit 1", "unit 1", "unit 1", "unit 2", "unit 2", "unit 2", "unit 2"))
-  expect_identical(object = x$year, expected = c("year 1", "year 1", "year 2", "year 2", "year 1", "year 1", "year 2", "year 2"))
-  expect_identical(object = x$commodities, expected = c("maize", "soybean", "maize", "soybean", "maize", "soybean", "maize", "soybean"))
-  expect_identical(object = x$harvested, expected = c(1121, 1111, 1221, 1211, 2121, 2111, 2221, 2211))
-  expect_identical(object = x$production, expected = c(1122, 1112, 1222, 1212, 2122, 2112, 2222, 2212))
+  assertChoice(x = units, choices = c(1:3))
+
+  if(units == 1){
+
+    expect_tibble(x = x, any.missing = FALSE, nrows = 4, ncols = 5)
+    expect_names(x = colnames(x), permutation.of =c("territories", "year", "commodities", "harvested", "production") )
+    expect_identical(object = x$territories, expected = c("unit 1", "unit 1", "unit 1", "unit 1"))
+    expect_identical(object = x$year, expected = c("year 1", "year 1", "year 2", "year 2"))
+    expect_identical(object = x$commodities, expected = c("maize", "soybean", "maize", "soybean"))
+    expect_identical(object = x$harvested, expected = c(1121, 1111, 1221, 1211))
+    expect_identical(object = x$production, expected = c(1122, 1112, 1222, 1212))
+
+  } else if(units == 2){
+
+    expect_tibble(x = x, any.missing = FALSE, nrows = 8, ncols = 5)
+    expect_names(x = colnames(x), permutation.of =c("territories", "year", "commodities", "harvested", "production") )
+    expect_identical(object = x$territories, expected = c("unit 1", "unit 1", "unit 1", "unit 1", "unit 2", "unit 2", "unit 2", "unit 2"))
+    expect_identical(object = x$year, expected = c("year 1", "year 1", "year 2", "year 2", "year 1", "year 1", "year 2", "year 2"))
+    expect_identical(object = x$commodities, expected = c("maize", "soybean", "maize", "soybean", "maize", "soybean", "maize", "soybean"))
+    expect_identical(object = x$harvested, expected = c(1121, 1111, 1221, 1211, 2121, 2111, 2221, 2211))
+    expect_identical(object = x$production, expected = c(1122, 1112, 1222, 1212, 2122, 2112, 2222, 2212))
+
+  } else if(units == 3){
+
+    expect_tibble(x = x, any.missing = FALSE, nrows = 12, ncols = 5)
+    expect_names(x = colnames(x), permutation.of = c("territories", "year", "commodities", "harvested", "production") )
+    expect_identical(object = x$territories, expected = c("unit 1", "unit 1", "unit 1", "unit 1", "unit 2", "unit 2", "unit 2", "unit 2", "unit 3", "unit 3", "unit 3", "unit 3"))
+    expect_identical(object = x$year, expected = c("year 1", "year 1", "year 2", "year 2", "year 1", "year 1", "year 2", "year 2", "year 1", "year 1", "year 2", "year 2"))
+    expect_identical(object = x$commodities, expected = c("maize", "soybean", "maize", "soybean", "maize", "soybean", "maize", "soybean", "maize", "soybean", "maize", "soybean"))
+    expect_identical(object = x$harvested, expected = c(1121, 1111, 1221, 1211, 2121, 2111, 2221, 2211, 3121, 3111, 3221, 3211))
+    expect_identical(object = x$production, expected = c(1122, 1112, 1222, 1212, 2122, 2112, 2222, 2212, 3122, 3112, 3222, 3212))
+
+  }
 }
