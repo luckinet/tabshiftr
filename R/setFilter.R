9724176c54ef2c758eaa94fe8659ec1ee8118a3c
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2021-05-29 21:13:14 +0200

    new function

--- R/setFilter.R
+++ R/setFilter.R
@@ -1,18 +1,20 @@
-#' Set filters to mask a table
+#' Set filters for reorganising
 #'
-#' This function allows to specify additional rules to (un-)select certain rows
-#' or columns.
+#' This function allows to specify additional rules to filter certain rows or
+#' columns.
 #' @param schema [\code{schema(1)}]\cr In case this information is added to an
 #'   already existing schema, provide that schema here (overwrites previous
 #'   information).
 #' @param columns [\code{integerish(.)}]\cr the columns to ingore.
 #' @param rows [\code{integerish(.)}]\cr the rows to ignore.
-#' @details Currently the functionality is restricted to filtering out columns
-#'   and rows globally across the whole table.
+#' @param invert [\code{logical(1)}]\cr whether or not to invert the values
+#'   provided in \code{columns} and \code{rows}.
+#' @details Currently the functionality is restricted to masking out columns and
+#'   rows globally across the whole table.
 #' @importFrom checkmate assertClass testIntegerish testClass
 #' @export
 
-setFilter <- function(schema = NULL, columns = NULL, rows = NULL){
+setFilter <- function(schema = NULL, columns = NULL, rows = NULL, invert = FALSE){
 
   # assertions ----
   assertClass(x = schema, classes = "schema", null.ok = TRUE)
@@ -34,6 +36,8 @@ setFilter <- function(schema = NULL, columns = NULL, rows = NULL){
   if(!is.null(columns)){
     schema@filter$col <- columns
   }
+
+  schema@filter$invert <- invert
 
   return(schema)
 
