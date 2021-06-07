001e81d1010a4e777ed9ddc56d11b5a59aa4fae8
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2021-06-07 15:59:29 +0200

    discard error management for now and include function call in tests

--- R/setIDVar.R
+++ R/setIDVar.R
@@ -43,10 +43,12 @@ setIDVar <- function(schema = NULL, name = NULL, value = NULL, columns = NULL,
   assertCharacter(x = name, len = 1, any.missing = FALSE)
   colInt <- testIntegerish(x = columns, lower = 1, min.len = 1, null.ok = TRUE)
   colQuo <- testClass(x = columns, classes = "quosure")
-  assert(colInt, colQuo)
+  colFun <- testClass(x = columns, classes = "name")
+  assert(colInt, colQuo, colFun)
   rowInt <- testIntegerish(x = rows, lower = 1, min.len = 1, null.ok = TRUE)
   rowQuo <- testClass(x = rows, classes = "quosure")
-  assert(rowInt, rowQuo)
+  rowFun <- testClass(x = rows, classes = "name")
+  assert(rowInt, rowQuo, rowFun)
   assertCharacter(x = value, len = 1, any.missing = FALSE, null.ok = TRUE)
   assertCharacter(x = split, len = 1, any.missing = FALSE, null.ok = TRUE)
   assertCharacter(x = merge, len = 1, any.missing = FALSE, null.ok = TRUE)
@@ -67,73 +69,73 @@ setIDVar <- function(schema = NULL, name = NULL, value = NULL, columns = NULL,
   }))
 
   # error management ----
-  if(!is.null(columns)){
-    # ensure that a row is set, in case the variable is contained in several columns
-    if(nClusters == 1){
-      if(length(columns) > 1){
-        if(is.null(rows)){
-          if(is.null(merge)){
-            message("  -> the variable '", name, "' is wide (i.e., in several columns), but no row with the names, nor the merge option is set.")
-          }
-        }
-      } else{
-        if(!is.null(rows)){
-          message("  -> 'row' is set for the variable '", name, "', even though it is not needed.")
-        }
-      }
-    }
-    # ensure that a split expression is set, in case the variable is contained in a column that already contains another variable
-    if(!colQuo){
-      if(any(columns %in% prevIDcols)){
-        if(is.null(split)){
-          message("  -> the variable '", name, "' is in a column (", paste(columns, collapse = ", "), ") that already contains another variable, but no split-expression is set.")
-        }
-      }
-    }
-  } else{
-    # if(!is.null(rows)){
-    #   message("  -> 'rows' is set for the variable '", name, "', even though it is not needed.")
-    # }
-  }
+  # if(!is.null(columns)){
+  #   # ensure that a row is set, in case the variable is contained in several columns
+  #   if(nClusters == 1){
+  #     if(length(columns) > 1){
+  #       if(is.null(rows)){
+  #         if(is.null(merge)){
+  #           message("  -> the variable '", name, "' is wide (i.e., in several columns), but no row with the names, nor the merge option is set.")
+  #         }
+  #       }
+  #     } else{
+  #       if(!is.null(rows)){
+  #         message("  -> 'row' is set for the variable '", name, "', even though it is not needed.")
+  #       }
+  #     }
+  #   }
+  #   # ensure that a split expression is set, in case the variable is contained in a column that already contains another variable
+  #   if(!colQuo){
+  #     if(any(columns %in% prevIDcols)){
+  #       if(is.null(split)){
+  #         message("  -> the variable '", name, "' is in a column (", paste(columns, collapse = ", "), ") that already contains another variable, but no split-expression is set.")
+  #       }
+  #     }
+  #   }
+  # } else{
+  #   # if(!is.null(rows)){
+  #   #   message("  -> 'rows' is set for the variable '", name, "', even though it is not needed.")
+  #   # }
+  # }
 
   # ensure that split results in a non-empty value
-  if(!is.null(split)){
-    if(is.null(columns)){
-      message("  -> the variable '", name, "' has a split-expression, but no column is set.")
-    } else {
-      # test that the split expression doesn't lead to an empty value
-      # recently not yet defined to have the input table in an environment for those "in-situ" tests
-    }
-  }
+  # if(!is.null(split)){
+  #   if(is.null(columns)){
+  #     message("  -> the variable '", name, "' has a split-expression, but no column is set.")
+  #   } else {
+  #     # test that the split expression doesn't lead to an empty value
+  #     # recently not yet defined to have the input table in an environment for those "in-situ" tests
+  #   }
+  # }
 
   # ensure that when not using 'value', either columns or rows is set
-  if(is.null(value)){
-    if(is.null(columns) & is.null(rows)){
-      message("  -> for the variable '", name, "' there is neither an explicit 'value' set, nor are there any column(s) (and rows).")
-    }
-  }
+  # if(is.null(value)){
+  #   if(is.null(columns) & is.null(rows)){
+  #     message("  -> for the variable '", name, "' there is neither an explicit 'value' set, nor are there any column(s) (and rows).")
+  #   }
+  # }
 
   # in case the user thought that it's sufficient to specify a row
-  if(!is.null(rows)){
-    if(is.null(columns)){
-      message("  -> in case the variable '", name, "' is in several columns, set first those columns and then the row of the variable names.")
-    } else{
-      # test that the column/row combination (here the variable names should be) leads to non-empty character values
-
-    }
-  }
+  # if(!is.null(rows)){
+  #   if(is.null(columns)){
+  #     message("  -> in case the variable '", name, "' is in several columns, set first those columns and then the row of the variable names.")
+  #   } else{
+  #     # test that the column/row combination (here the variable names should be) leads to non-empty character values
+  #
+  #   }
+  # }
 
   # ensure that relative values are still within the cluster
-  if(nClusters != 0){
-    if(relative){
-      if(!is.null(schema@clusters$width)){
-
-      }
-      if(!is.null(schema@clusters$height)){
-
-      }
-    }
-  }
+  # if(nClusters != 0){
+  #   if(relative){
+  #     if(!is.null(schema@clusters$width)){
+  #
+  #     }
+  #     if(!is.null(schema@clusters$height)){
+  #
+  #     }
+  #   }
+  # }
 
   # ensure that if dist = TRUE, values are absolute and the defined fields contain valid values
 
