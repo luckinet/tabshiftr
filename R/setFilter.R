a8f228a6eafeb0864bef06527e164d48659c04a1
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2021-06-21 13:47:56 +0200

    be more precise, filter is only for rows

--- R/setFilter.R
+++ R/setFilter.R
@@ -1,11 +1,9 @@
-#' Set filters for reorganising
+#' Set filters
 #'
-#' This function allows to specify additional rules to filter certain rows or
-#' columns.
+#' This function allows to specify additional rules to filter certain rows
 #' @param schema [\code{schema(1)}]\cr In case this information is added to an
 #'   already existing schema, provide that schema here (overwrites previous
 #'   information).
-#' @param columns [\code{integerish(.)}]\cr the columns to ingore.
 #' @param rows [\code{integerish(.)}]\cr the rows to ignore.
 #' @param invert [\code{logical(1)}]\cr whether or not to invert the values
 #'   provided in \code{columns} and \code{rows}.
@@ -14,13 +12,10 @@
 #' @importFrom checkmate assertClass testIntegerish testClass
 #' @export
 
-setFilter <- function(schema = NULL, columns = NULL, rows = NULL, invert = FALSE){
+setFilter <- function(schema = NULL, rows = NULL, invert = FALSE){
 
   # assertions ----
   assertClass(x = schema, classes = "schema", null.ok = TRUE)
-  colInt <- testIntegerish(x = columns, lower = 1, min.len = 1, null.ok = TRUE)
-  colQuo <- testClass(x = columns, classes = "quosure")
-  assert(colInt, colQuo)
   rowInt <- testIntegerish(x = rows, lower = 1, min.len = 1, null.ok = TRUE)
   rowQuo <- testClass(x = rows, classes = "quosure")
   assert(rowInt, rowQuo)
@@ -31,10 +26,6 @@ setFilter <- function(schema = NULL, columns = NULL, rows = NULL, invert = FALSE
 
   if(!is.null(rows)){
     schema@filter$row <- rows
-  }
-
-  if(!is.null(columns)){
-    schema@filter$col <- columns
   }
 
   schema@filter$invert <- invert
