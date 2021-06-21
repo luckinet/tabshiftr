48bd0b7b04e8cb5282832d9d10fc1dee9f0e0084
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2021-06-21 14:09:12 +0200

    rename to .find

--- tests/testthat/test-find.R
+++ tests/testthat/test-find.R
@@ -3,10 +3,10 @@ library(testthat)
 library(readr)
 library(magrittr)
 library(checkmate)
-context("find")
+context(".find")
 
 
-test_that("find columns based on regular expressions", {
+test_that(".find columns based on regular expressions", {
 
   input <- read_csv(paste0(system.file("test_datasets",
                                        package = "tabshiftr",
@@ -14,11 +14,11 @@ test_that("find columns based on regular expressions", {
                     col_names = FALSE)
 
   schema <-
-    setIDVar(name = "territories", columns = find("unit*")) %>%
-    setIDVar(name = "year", columns = find("year*"), rows = find("year*")) %>%
-    setIDVar(name = "commodities", columns = find("soy|mai*"), rows = find("soy|mai*")) %>%
-    setObsVar(name = "harvested", columns = find("harv*"), top = find("harv*")) %>%
-    setObsVar(name = "production", columns = find("prod*"), top = find("prod*"))
+    setIDVar(name = "territories", columns = .find("unit*")) %>%
+    setIDVar(name = "year", columns = .find("year*"), rows = .find("year*")) %>%
+    setIDVar(name = "commodities", columns = .find("soy|mai*"), rows = .find("soy|mai*")) %>%
+    setObsVar(name = "harvested", columns = .find("harv*"), top = .find("harv*")) %>%
+    setObsVar(name = "production", columns = .find("prod*"), top = .find("prod*"))
 
   .expect_valid_table(x = reorganise(input = input, schema = schema), units = 2)
 
