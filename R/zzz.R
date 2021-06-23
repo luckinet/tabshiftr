114dc415d9fbaaa67493f724e5537e0c81cda334
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2021-06-23 19:25:08 +0200

    more global variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,2 @@
 globalVariables(c("key", "values", "meta_default", "schema_default", "rn", "val", ".", "name",
-                  "count", "value", "x", "where"))
+                  "count", "value", "x", "where", "rowname", "variable"))
