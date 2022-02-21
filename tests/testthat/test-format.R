14783e16630731cc9e5071681c87c8e96aa6befe
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2022-02-21 13:14:37 +0100

    new test to assert format (only flags for now)

---
+++ tests/testthat/test-format.R
@@ -0,0 +1,26 @@
+library(tabshiftr)
+library(testthat)
+library(checkmate)
+context("setFormat")
+
+
+test_that("extract flags into their own column", {
+
+  input <- tabs2shift$tidy
+  input$X5[3] <- paste0(input$X5[3], "p")
+  input$X6[7] <- paste0(input$X6[7], " $")
+
+  schema <-
+    setFormat(flags = tibble(flag = c("p", "$"), value = c("provisional", "only applicable in case of X"))) %>%
+    setIDVar(name = "territories", columns = 1) %>%
+    setIDVar(name = "year", columns = 2) %>%
+    setIDVar(name = "commodities", columns = 3) %>%
+    setObsVar(name = "harvested", columns = 5) %>%
+    setObsVar(name = "production", columns = 6)
+
+  .expect_valid_table(x = reorganise(input = input, schema = schema), units = 2, flags = TRUE)
+
+})
+
+
+
