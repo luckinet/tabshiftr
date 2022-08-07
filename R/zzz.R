b94262f5c5241a1f35289bc8b36c6410ef67a5f9
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2022-08-07 18:40:46 +0200

    more global variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,3 @@
 globalVariables(c("key", "values", "meta_default", "schema_default", "rn", "val", ".", "name",
-                  "count", "value", "x", "where", "rowname", "variable"))
+                  "count", "value", "x", "where", "rowname", "variable", "grps", "targetCols"
+                  ))
