32f79a115223a02414a8f77996a6722d9099afe5
EhrmannS <steffen.ehrmann@posteo.de>
2026-05-16 13:35:47 +0200

    re-include package description

---
+++ R/tabshiftr-package.R
@@ -0,0 +1,23 @@
+#' tabshiftr: Reshape Disorganised Messy Data
+#'
+#' The tabshiftr package helps the user to build and register schema
+#' descriptions of disorganised (messy) tables. Disorganised tables are tables
+#' that are not in a topologically coherent form, where packages such as
+#' \pkg{tidyr} could be used for reshaping. The schema description documents
+#' the arrangement of input tables and is used to reshape them into a
+#' standardised (tidy) output format.
+#'
+#' @author \strong{Maintainer, Author}: Steffen Ehrmann
+#'   \email{steffen.ehrmann@posteo.de}
+#'
+#' @seealso \itemize{
+#'   \item Package website:
+#'         \href{https://luckinet.github.io/tabshiftr/}{https://luckinet.github.io/tabshiftr/}
+#'   \item Github project:
+#'         \href{https://github.com/luckinet/tabshiftr}{https://github.com/luckinet/tabshiftr}
+#'   \item Report bugs:
+#'         \href{https://github.com/luckinet/tabshiftr/issues}{https://github.com/luckinet/tabshiftr/issues}
+#' }
+#' @docType package
+#' @name tabshiftr
+'_PACKAGE'
